e31c84397d9cb6872f1df9be001da1e9712d4423
eunbi99 <silverbi99@naver.com>
2021-06-17 10:54:16 +0900

    Init commit

---
+++ src/main/java/com/homegym/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.homegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/user/*")
+public class MemberController {
+
+	@GetMapping("/mp_main")
+	public String mypage() {
+		return "user/mp_main";
+	}
+}
