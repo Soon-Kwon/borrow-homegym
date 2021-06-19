a26f795a1e753a4b0a4b0343f74471c4366e47c7
YIM-SSOO <81502053+YIM-SSOO@users.noreply.github.com>
2021-06-19 18:11:13 +0900

    컨트롤러 작업 (servlet-context.xml / root-context.xml 에 추가 매핑)

--- src/main/java/com/homegym/biz/trainer_board/controller/TrainerBoardController.java
+++ src/main/java/com/homegym/biz/trainer_board/controller/TrainerBoardController.java
@@ -1,13 +1,28 @@
 package com.homegym.biz.trainer_board.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-@RequestMapping("/user/*")
+@RequestMapping("/trainer/*")
 public class TrainerBoardController {
-
+	
+	//마이페이지 메인  이동
+		@GetMapping("/test")
+		public String mypage(HttpServletRequest request, HttpSession session) {
+			
+			
+			System.out.println("여기에 들어오는가?");
+			
+			//String memberId = request.getParameter("memberId");
+			//session.setAttribute("memberId", memberId);
+			return "trainer/test";
+		}
 }
